43767e901692a6e9b6fd329734d5afe98c6c1e0e
Denis Gathondu <denis.gathondu@andela.com>
2017-06-28 10:26:20 +0300

    updates to allow to work with morph.io

--- healthtools/config.py
+++ healthtools/config.py
@@ -12,17 +12,17 @@ AWS = {
     "aws_access_key_id": os.getenv("MORPH_AWS_ACCESS_KEY"),
     "aws_secret_access_key": os.getenv("MORPH_AWS_SECRET_KEY"),
     "region_name": os.getenv("MORPH_AWS_REGION", "eu-west-1"),
-    "s3_bucket": os.getenv("S3_BUCKET", None)
+    "s3_bucket": os.getenv("MORPH_S3_BUCKET", None)
 }
 
 ES = {
-    "host": os.getenv("ES_HOST", "127.0.0.1"),
-    "port": os.getenv("ES_PORT", "9200"),
+    "host": os.getenv("MORPH_ES_HOST", "127.0.0.1"),
+    "port": os.getenv("MORPH_ES_PORT", "9200"),
     "index": "healthtools"
 }
 
 SLACK = {
-    "url": os.getenv("WEBHOOK_URL")
+    "url": os.getenv("MORPH_WEBHOOK_URL")
 }
 
 TEST_DIR = os.getcwd() + "/healthtools/tests"
