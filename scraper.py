0427201e70445b2481a8330a7884ba82c251868a
David Lemayian <dlemayian@gmail.com>
2017-07-23 12:20:42 +0300

    Added inline documentation to scraper.py

--- scraper.py
+++ scraper.py
@@ -13,17 +13,43 @@ if __name__ == "__main__":
     foreign_doctors_scraper = ForeignDoctorsScraper()
     clinical_officers_scraper = ClinicalOfficersScraper()
     healthfacilities_scraper = HealthFacilitiesScraper()
+
     nhif_inpatient_scraper = NhifInpatientScraper()
     nhif_outpatient_scraper = NhifOutpatientScraper()
     nhif_outpatient_cs_scraper = NhifOutpatientCsScraper()
 
-    # scraping you softly with these bots...
+    # Run the scrapers
+
+    '''
+    Doctors Scraper
+    ---------------
+    Doctors are a combination of local and foreign doctors. If the local
+    doctors' scraper fails, we shouldn't scrape the foreign doctors.
+    '''
     doctors_result = doctors_scraper.scrape_site()
     if doctors_result:
-        foreign_doctors_scraper.document_id = len(doctors_result)/ 2
+        foreign_doctors_scraper.document_id = len(doctors_result)
         foreign_docs_result = foreign_doctors_scraper.scrape_site()
+
+    '''
+    Clinical Officers Scraper
+    -------------------------
+    Scrapes the clinical officers website.
+    '''
     clinical_officers_result = clinical_officers_scraper.scrape_site()
+
+    '''
+    Health Facilities Scraper
+    -------------------------
+    Scrapes the government's Kenya Health Facilities Master List.
+    '''
     healthfacilities_result = healthfacilities_scraper.scrape_data()
+
+    '''
+    NHIF Scraper
+    -------------------------
+    Scrapes the NHIF website for accredited hospital / facitilities.
+    '''
     nhif_inpatient_result = nhif_inpatient_scraper.scrape_site()
     nhif_outpatient_result = nhif_outpatient_scraper.scrape_site()
     nhif_outpatient_cs_result = nhif_outpatient_cs_scraper.scrape_site()
