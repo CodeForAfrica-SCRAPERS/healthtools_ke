5ab9a7ce00fd860b08127577d0b71f884b1e9355
Mayowa Makinde <mayowa.makinde@andela.com>
2017-10-10 17:28:40 +0100

    Break the heath-facilities scraping into chunks

--- healthtools/scrapers/health_facilities.py
+++ healthtools/scrapers/health_facilities.py
@@ -1,23 +1,13 @@
 import json
+import math
 from healthtools.scrapers.base_scraper import Scraper
 from healthtools.config import SMALL_BATCH_HF
 import requests
 from datetime import datetime
 
 TOKEN_URL = "http://api.kmhfl.health.go.ke/o/token/"
-SEARCH_URL = "http://api.kmhfl.health.go.ke/api/facilities/material/?page_size={}&" \
-             "fields=id,regulatory_status_name,facility_type_name,facility_type_parent,owner_name,owner_type_name," \
-             "owner_type,operation_status_name,county,constituency,constituency_name,ward_name,average_rating," \
-             "facility_services,is_approved,has_edits,latest_update,regulatory_body_name,owner,date_requested," \
-             "date_approved,latest_approval_or_rejection,sub_county_name,sub_county_id,county_name,constituency_id," \
-             "county_id,keph_level_name,facility_contacts,coordinates,lat_long,latest_approval,county_code,constituency_code" \
-             ",ward_code,service_catalogue_active,facility_units,officer_in_charge,created,updated,deleted,active,search," \
-             "name,official_name,code,registration_number,abbreviation,description,number_of_beds,number_of_cots," \
-             "open_whole_day,open_public_holidays,open_normal_day,open_weekends,open_late_night,is_classified," \
-             "is_published,regulated,approved,rejected,bank_name,branch_name,bank_account,facility_catchment_population," \
-             "town_name,nearest_landmark,plot_number,location_desc,closed,closed_date,closing_reason,date_established," \
-             "license_number,created_by,updated_by,facility_type,operation_status,ward,parent,regulatory_body," \
-             "keph_level,sub_county,town,regulation_status,contacts&format=json"
+SEARCH_URL = "http://api.kmhfl.health.go.ke/api/facilities/material/\
+?format=json&page={}&page_size=5000"
 
 
 class HealthFacilitiesScraper(Scraper):
@@ -28,17 +18,23 @@ class HealthFacilitiesScraper(Scraper):
         self.data_archive_key = "archive/health_facilities-{}.json"
 
         self.access_token = None
+        self.total_pages = 0
 
-    def scrape_site(self):
-        self.get_token()
-        self.get_data()
+    def scrape_site(self, page_no=1):
+        if page_no == 1:
+            self.get_token()
+
+        self.get_data(page_no)
 
         if self.results:
-            self.archive_data(json.dumps(self.results))
-            self.elasticsearch_delete_docs()
+            if page_no == 1:
+                self.elasticsearch_delete_docs()
             self.elasticsearch_index(self.results_es)
-
-            return self.results
+            if page_no < self.total_pages:
+                self.scrape_site(page_no=page_no + 1)
+            else:
+                self.archive_data(json.dumps(self.results))
+                return self.results
 
     def get_token(self):
         headers = {"Content-Type": "application/x-www-form-urlencoded"}
@@ -61,14 +57,18 @@ class HealthFacilitiesScraper(Scraper):
             }
             self.print_error(error)
 
-    def get_data(self):
+    def get_data(self, page_no):
+        self.results_es = []
         try:
-            url = SEARCH_URL.format(1000000)
+            url = SEARCH_URL.format(page_no)
             if self.small_batch:
                 url = SEARCH_URL.format(SMALL_BATCH_HF)
             headers = {"Authorization": "Bearer " + self.access_token}
             r = requests.get(url, headers=headers)
             data = r.json()
+            total_hits = data["count"]
+            self.total_pages = int(math.ceil(total_hits/float(5000)))
+
             for entry in data["results"]:
                 entry["id"] = self.doc_id
                 meta, entry = self.elasticsearch_format(entry)
