d8cf04da256af9906c536c8229dbb052def39169
Mayowa Makinde <mayowa.makinde@andela.com>
2017-10-28 19:25:33 +0100

    re-index previous data if scraping fails

--- healthtools/scrapers/health_facilities.py
+++ healthtools/scrapers/health_facilities.py
@@ -1,16 +1,30 @@
 import json
 import math
 import logging
+from werkzeug.local import LocalProxy
+from elasticsearch import Elasticsearch, RequestsHttpConnection
 from healthtools.scrapers.base_scraper import Scraper
 from healthtools.config import SMALL_BATCH_HF
+from healthtools.config import ES
 import requests
 from datetime import datetime
 
 log = logging.getLogger(__name__)
 
 TOKEN_URL = "http://api.kmhfl.health.go.ke/o/token/"
-SEARCH_URL = "http://api.kmhfl.health.go.ke/api/facilities/material/\
-?format=json&page={}&page_size=5000"
+SEARCH_URL = "http://api.kmhfl.health.go.ke/api/facilities/material/?page_size={}&" \
+             "fields=id,regulatory_status_name,facility_type_name,facility_type_parent,owner_name,owner_type_name," \
+             "owner_type,operation_status_name,county,constituency,constituency_name,ward_name,average_rating," \
+             "facility_services,is_approved,has_edits,latest_update,regulatory_body_name,owner,date_requested," \
+             "date_approved,latest_approval_or_rejection,sub_county_name,sub_county_id,county_name,constituency_id," \
+             "county_id,keph_level_name,facility_contacts,coordinates,lat_long,latest_approval,county_code,constituency_code" \
+             ",ward_code,service_catalogue_active,facility_units,officer_in_charge,created,updated,deleted,active,search," \
+             "name,official_name,code,registration_number,abbreviation,description,number_of_beds,number_of_cots," \
+             "open_whole_day,open_public_holidays,open_normal_day,open_weekends,open_late_night,is_classified," \
+             "is_published,regulated,approved,rejected,bank_name,branch_name,bank_account,facility_catchment_population," \
+             "town_name,nearest_landmark,plot_number,location_desc,closed,closed_date,closing_reason,date_established," \
+             "license_number,created_by,updated_by,facility_type,operation_status,ward,parent,regulatory_body," \
+             "keph_level,sub_county,town,regulation_status,contacts&format=json"
 
 
 class HealthFacilitiesScraper(Scraper):
@@ -21,21 +35,54 @@ class HealthFacilitiesScraper(Scraper):
         self.data_archive_key = "archive/health_facilities-{}.json"
 
         self.access_token = None
-        self.total_pages = 0
 
-    def scrape_site(self, page_no=1):
-        self.get_token()
-        self.get_data(page_no)
-        if self.results:
-            self.elasticsearch_delete_docs()
-            self.elasticsearch_index(self.results_es)
+    def index_to_es(self, current_result, isExisting=False):
+        current_page_data = []
+        page_count = 1
+        total_pages = int(math.ceil(len(current_result) / float(9000)))
+        while page_count < total_pages:
+            start_index = (page_count - 1) * 9000
+            end_index = start_index + 9000
+            current_page_data = current_result[start_index: end_index]
+            if isExisting:
+                page_data = []
+                current_page_data = [curIndex.get('_source') for curIndex in current_page_data]
+                for hit in current_page_data:
+                    hit["id"] = self.doc_id
+                    meta, entry = self.elasticsearch_format(hit, True)
+                    page_data.append(meta)
+                    page_data.append(entry)
+
+                    self.doc_id += 1
+                current_page_data = page_data
+            self.elasticsearch_index(current_page_data)
+            page_count += 1
+
+    def scrape_site(self):
+        existing_index_data = []
+        es = Elasticsearch("{}:{}".format(ES["host"], ES["port"]))
+        doc = {
+            'size': 10000,
+            'query': {
+                'match_all': {}
+            }
+        }
+        existing_index_data = es.search(
+            index=ES["index"], doc_type=self.es_doc, body=doc, scroll='1m')
+        existing_index_data = existing_index_data['hits']['hits']
         try:
-            while page_no < self.total_pages:
-                self.get_data(page_no + 1)
-                if self.results:
-                    self.elasticsearch_index(self.results_es)
-                page_no += 1
+            self.get_token()
+            self.get_data()
+            current_result = self.results_es
+            self.index_to_es(current_result)
+            if current_result:
+                self.archive_data(json.dumps(current_result))
+                self.elasticsearch_delete_docs()
+
         except Exception as err:
+            current_result = existing_index_data
+            self.index_to_es(current_result, True)
+            self.archive_data(json.dumps(current_result))
             error = {
                 "ERROR": "scrape_site()",
                 "MESSAGE": str(err)
@@ -53,47 +100,28 @@ class HealthFacilitiesScraper(Scraper):
             "client_id": "xMddOofHI0jOKboVxdoKAXWKpkEQAP0TuloGpfj5",
             "client_secret": "PHrUzCRFm9558DGa6Fh1hEvSCh3C9Lijfq8sbCMZhZqmANYV5ZP04mUXGJdsrZLXuZG4VCmvjShdKHwU6IRmPQld5LDzvJoguEP8AAXGJhrqfLnmtFXU3x2FO1nWLxUx"
         }
-        try:
-            response = requests.post(TOKEN_URL, data=data, headers=headers)
-            self.access_token = json.loads(response.text)["access_token"]
-            log.info("[%s] Access token received.",
-                datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-        except Exception as err:
-            error = {
-                "ERROR": "get_token()",
-                "MESSAGE": str(err)
-            }
-            self.print_error(error)
+        response = requests.post(TOKEN_URL, data=data, headers=headers)
+        self.access_token = json.loads(response.text)["access_token"]
+        log.info("[%s] Access token received.",
+                 datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
-    def get_data(self, page_no):
-        self.results_es = []
-        try:
-            url = SEARCH_URL.format(page_no)
-            if self.small_batch:
-                url = SEARCH_URL.format(SMALL_BATCH_HF)
-            headers = {"Authorization": "Bearer " + self.access_token}
-            r = requests.get(url, headers=headers)
-            data = r.json()
-            total_hits = data["count"]
-            self.total_pages = int(math.ceil(total_hits/float(5000)))
+    def get_data(self):
+        url = SEARCH_URL.format(1000000)
+        if self.small_batch:
+            url = SEARCH_URL.format(SMALL_BATCH_HF)
+        headers = {"Authorization": "Bearer " + self.access_token}
+        r = requests.get(url, headers=headers)
+        data = r.json()
+        for entry in data["results"]:
+            entry["id"] = self.doc_id
+            meta, entry = self.elasticsearch_format(entry)
+            self.results_es.append(meta)
+            self.results_es.append(entry)
+            self.results.append(entry)
 
-            for entry in data["results"]:
-                entry["id"] = self.doc_id
-                meta, entry = self.elasticsearch_format(entry)
-                self.results_es.append(meta)
-                self.results_es.append(entry)
-                self.results.append(entry)
+            self.doc_id += 1
 
-                self.doc_id += 1
-
-        except Exception as err:
-            error = {
-                "ERROR": "get_data()",
-                "MESSAGE": str(err)
-            }
-            self.print_error(error)
-
-    def elasticsearch_format(self, entry):
+    def elasticsearch_format(self, entry, isExisting=False):
         meta_dict = {
             "index": {
                 "_index": self.es_index,
@@ -101,5 +129,7 @@ class HealthFacilitiesScraper(Scraper):
                 "_id": self.doc_id
             }
         }
-        entry["ward_name"] = entry["ward_name"].decode("string_escape").replace("\\", "")
+        if not isExisting:
+            entry["ward_name"] = entry["ward_name"].decode(
+                "string_escape").replace("\\", "")
         return meta_dict, entry
--- scraper.py
+++ scraper.py
@@ -14,14 +14,14 @@ logging.basicConfig(level=logging.INFO)
 if __name__ == "__main__":
 
     # Initialize the Scrapers
-    doctors_scraper = DoctorsScraper()
-    foreign_doctors_scraper = ForeignDoctorsScraper()
-    clinical_officers_scraper = ClinicalOfficersScraper()
+    # doctors_scraper = DoctorsScraper()
+    # foreign_doctors_scraper = ForeignDoctorsScraper()
+    # clinical_officers_scraper = ClinicalOfficersScraper()
     healthfacilities_scraper = HealthFacilitiesScraper()
 
-    nhif_inpatient_scraper = NhifInpatientScraper()
-    nhif_outpatient_scraper = NhifOutpatientScraper()
-    nhif_outpatient_cs_scraper = NhifOutpatientCsScraper()
+    # nhif_inpatient_scraper = NhifInpatientScraper()
+    # nhif_outpatient_scraper = NhifOutpatientScraper()
+    # nhif_outpatient_cs_scraper = NhifOutpatientCsScraper()
 
     # Run the scrapers
 
@@ -32,17 +32,17 @@ if __name__ == "__main__":
     doctors' scraper fails, we shouldn't scrape the foreign doctors.
     '''
 
-    doctors_result = doctors_scraper.run_scraper()
-    if doctors_result:
-        foreign_doctors_scraper.doc_id = len(doctors_result)
-        foreign_docs_result = foreign_doctors_scraper.run_scraper()
+    # doctors_result = doctors_scraper.run_scraper()
+    # if doctors_result:
+    #     foreign_doctors_scraper.doc_id = len(doctors_result)
+    #     foreign_docs_result = foreign_doctors_scraper.run_scraper()
 
     '''
     Clinical Officers Scraper
     -------------------------
     Scrapes the clinical officers website.
     '''
-    clinical_officers_result = clinical_officers_scraper.run_scraper()
+    # clinical_officers_result = clinical_officers_scraper.run_scraper()
 
     '''
     Health Facilities Scraper
@@ -56,6 +56,6 @@ if __name__ == "__main__":
     -------------------------
     Scrapes the NHIF website for accredited hospital / facitilities.
     '''
-    nhif_inpatient_result = nhif_inpatient_scraper.run_scraper()
-    nhif_outpatient_result = nhif_outpatient_scraper.run_scraper()
-    nhif_outpatient_cs_result = nhif_outpatient_cs_scraper.run_scraper()
+    # nhif_inpatient_result = nhif_inpatient_scraper.run_scraper()
+    # nhif_outpatient_result = nhif_outpatient_scraper.run_scraper()
+    # nhif_outpatient_cs_result = nhif_outpatient_cs_scraper.run_scraper()
